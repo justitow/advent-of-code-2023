c3668a63c160e0091feaf2285dfda5f1df9def94
justitow <justitow@gmail.com>
2023-12-08 21:52:41 -0800

    day day09

--- day09.py
+++ day09.py
@@ -1,9 +1,5 @@
 from collections import deque
 
-with open('day09.txt') as input_file:
-    puzzle_input = [deque([int(number) for number in line.strip().split(' ')]) for line in input_file]
-with open('day09.txt') as input_file:
-    puzzle_input_left = [deque([int(number) for number in line.strip().split(' ')]) for line in input_file]
 
 def reduce_delta(sequence):
     if sequence[0] == 0 and all(x == sequence[0] for x in sequence):
@@ -18,27 +14,19 @@ def reduce_delta(sequence):
         ans = curr + reduce_delta(new_sequence)
         return ans
 
-def reduce_delta_left(sequence):
-    if sequence[0] == 0 and all(x == sequence[0] for x in sequence):
-        return 0
-    else:
-        new_sequence = deque()
-        curr = sequence.pop()
-        while sequence:
-            prev = curr
-            curr = sequence.pop()
-            new_sequence.appendleft(prev - curr)
-        ans = curr - reduce_delta_left(new_sequence)
-        return ans
 
-
+with open('day09.txt') as input_file:
+    puzzle_input = [deque([int(number) for number in line.strip().split(' ')]) for line in input_file]
 
 ans_1 = 0
 for line in puzzle_input:
     ans_1 += reduce_delta(line)
 print(ans_1)
 
+with open('day09.txt') as input_file:
+    puzzle_input = [deque([int(number) for number in line.strip().split(' ')]) for line in input_file]
+
 ans_1 = 0
-for line in puzzle_input_left:
-    ans_1 += reduce_delta_left(line)
+for line in puzzle_input:
+    ans_1 += reduce_delta(deque(list(line)[::-1]))
 print(ans_1)
